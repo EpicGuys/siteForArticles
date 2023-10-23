19721f3b5cef29d1a1e91aa2b9b52e69d5cb8c1b
Stepashka369 <kardash-99@list.ru>
2023-10-23 11:03:53 +0300

    same changes in security

--- src/main/java/com/epicGuys/app/articles/config/SecurityConfiguration.java
+++ src/main/java/com/epicGuys/app/articles/config/SecurityConfiguration.java
@@ -1,19 +1,32 @@
 package com.epicGuys.app.articles.config;
 
 import com.epicGuys.app.articles.repository.UserDetailServiceImpl;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.filters.CorsFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration{
+    private final JwtTokenFilter jwtTokenFilter;
+
+    @Autowired
+    public SecurityConfiguration(JwtTokenFilter jwtTokenFilter) {
+        this.jwtTokenFilter = jwtTokenFilter;
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -25,6 +38,16 @@ public class SecurityConfiguration{
         return new BCryptPasswordEncoder();
     }
 
+    @Bean //????
+    public AuthenticationManager authenticationManager(){
+        return new AuthenticationManager() {
+            @Override
+            public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+                return null;
+            }
+        };
+    }
+
     @Bean
     public DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -33,27 +56,37 @@ public class SecurityConfiguration{
         return authProvider;
     }
 
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.authenticationProvider(authenticationProvider());
+//    @Bean
+//    public CorsFilter corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        configuration.setAllowCredentials(true);
+//        configuration.addAllowedOrigin("*");
+//        configuration.addAllowedHeader("*");
+//        configuration.addAllowedMethod("*");
+//        source.registerCorsConfiguration("/**", configuration);
+//        return (CorsFilter) source;
 //    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/registration", "/login").permitAll()
-                        .requestMatchers("/**").authenticated())
-                .formLogin((form) -> form.permitAll());
+                .csrf((object) -> object
+                        .disable())
+                .sessionManagement((object) -> object
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling((object) -> object
+                        .authenticationEntryPoint(((request, response, authException) -> {
+                            response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+                                    authException.getMessage());
+                        })))
+                .authorizeHttpRequests((object) -> object
+                        .requestMatchers("/registration").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
 
-
-        //        return http.authorizeRequests()
-//                .anyRequest().authenticated()
-//                .and()
-//                .formLogin().permitAll()
-//                .and()
-//                .logout().permitAll()
-//                .and().build();
     }
 }
--- src/main/java/com/epicGuys/app/articles/controllers/RegistrationController.java
+++ src/main/java/com/epicGuys/app/articles/controllers/RegistrationController.java
@@ -3,37 +3,82 @@ package com.epicGuys.app.articles.controllers;
 import com.epicGuys.app.articles.entity.Role;
 import com.epicGuys.app.articles.entity.User;
 import com.epicGuys.app.articles.repository.UserRepository;
+import com.epicGuys.app.articles.service.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 
+
 @RestController
+@RequestMapping(path = "api/public")
 public class RegistrationController {
+        private final AuthenticationManager authenticationManager;
+        private final JwtUtil jwtUtil;
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @GetMapping("/registration")
-    public String registration(@RequestParam String username, @RequestParam String password){
-        if(userRepository.getUserByUsername(username) != null){
-            return "User with this name already exists";
+        public RegistrationController(AuthenticationManager authenticationManager,
+                                      JwtUtil jwtUtil) {
+            this.authenticationManager = authenticationManager;
+            this.jwtUtil = jwtUtil;
         }
-        User user = new User();
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-        user.setUsername(username);
-        user.setPassword(bCryptPasswordEncoder.encode(password));
-        user.setEnabled(true);
-        user.setRoles(Collections.singleton(Role.ROLE_ADMIN));
-        userRepository.save(user);
-        return "registration successful";
+
+        @PostMapping("login")
+        public ResponseEntity login(@RequestBody User request) {
+            try {
+                Authentication authenticate = authenticationManager
+                        .authenticate(
+                                new UsernamePasswordAuthenticationToken(
+                                        request.getUsername(), request.getPassword()
+                                )
+                        );
+
+                User user = (User) authenticate.getPrincipal();
+
+                return new ResponseEntity(HttpStatus.OK);
+
+            } catch (BadCredentialsException ex) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
     }
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello";
-    }
+
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    @GetMapping("/registration")
+//    public String registration(@RequestParam String username, @RequestParam String password){
+//        if(userRepository.getUserByUsername(username) != null){
+//            return "User with this name already exists";
+//        }
+//        User user = new User();
+//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+//        user.setUsername(username);
+//        user.setPassword(bCryptPasswordEncoder.encode(password));
+//        user.setEnabled(true);
+//        user.setRoles(Collections.singleton(Role.ROLE_ADMIN));
+//        userRepository.save(user);
+//        return "registration successful";
+//    }
+//
+//    @PostMapping("/login")
+//    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+//        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+//        Authentication authentication = authenticationManager.authenticate(token);
+//        SecurityContextHolder.getContext().setAuthentication(authentication);
+//        return "redirect:/";
+//    }
+//
+//    @GetMapping("/hello")
+//    public String hello(){
+//        return "hello";
+//    }
 }
