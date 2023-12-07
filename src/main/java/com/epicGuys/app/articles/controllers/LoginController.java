a8e7bd5f72eba2bcc801822d7b56096e9417674a
Nikita Gill <gilnikita026@gmail.com>
2023-12-07 18:41:04 +0300

    Ready...EpicArticles v1.0

--- src/main/java/com/epicGuys/app/articles/controllers/LoginController.java
+++ src/main/java/com/epicGuys/app/articles/controllers/LoginController.java
@@ -1,12 +1,28 @@
 package com.epicGuys.app.articles.controllers;
 
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.epicGuys.app.articles.entity.Role;
+import com.epicGuys.app.articles.entity.User;
+import com.epicGuys.app.articles.service.UserService;
+
 @Controller
 public class LoginController {
+	
+	@Autowired
+	private UserService userService;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@RequestMapping("/login")
     public String getLogin(@RequestParam(value = "error", required = false) String error,
@@ -15,5 +31,19 @@ public class LoginController {
         model.addAttribute("error", error != null);
         model.addAttribute("logout", logout != null);
         return "login";
+    }
+	
+	@GetMapping("/registration")
+    public String registrationPage(@ModelAttribute("user") User user) {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String processRegistration(@ModelAttribute("user") User user) {
+    	user.setPassword(passwordEncoder.encode(user.getPassword()));
+		user.setEnabled(true);
+		user.setRoles(Set.of(Role.ROLE_WRITER));
+        userService.saveUser(user);
+        return "redirect:/login";
     }
 }
